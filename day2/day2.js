05b27180ccb305d061f909e2eeb0f119b787b52d
Alvaro Enrique Ruano <alvaro.ruano90@outlook.com>
2021-12-25 11:26:26 -0600

    Day2 completed

--- day2/day2.js
+++ day2/day2.js
@@ -31,10 +31,41 @@ part1 = () => {
         }
     });
 
-    console.log(`The final position is:`);
+    console.log(`The final position following Part1 instructions is:`);
     console.log(`\tHorizontal: ${horizontalPosition}`);
     console.log(`\tDepth: ${depthPosition}`);
     console.log(`The final answer is: ${depthPosition * horizontalPosition}`);
+    console.log(`\n`);
 }
 
 part1();
+
+part2 = () => {
+    horizontalPosition = 0;
+    depthPosition = 0;
+    aim = 0;
+
+    instructions.forEach(instruction => {
+        switch (instruction.direction) {
+            case "forward":
+                horizontalPosition += instruction.distance;
+                depthPosition += instruction.distance * aim;
+                break;
+            case "up":
+                aim -= instruction.distance;
+                break;
+            case "down":
+                aim += instruction.distance;
+                break;
+        }
+    });
+
+    console.log(`The final position following Part2 instructions is:`);
+    console.log(`\tHorizontal: ${horizontalPosition}`);
+    console.log(`\tDepth: ${depthPosition}`);
+    console.log(`\tAim: ${aim}`);
+    console.log(`The final answer is: ${depthPosition * horizontalPosition}`);
+    console.log(`\n`);
+}
+
+part2();
