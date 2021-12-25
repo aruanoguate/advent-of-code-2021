b0d23e1a723ba50621bb561fbe0e5a89510b52bd
Alvaro Enrique Ruano <alvaro.ruano90@outlook.com>
2021-12-25 10:40:27 -0600

    Encapsulate part 1

--- day1/day1.js
+++ day1/day1.js
@@ -1,17 +1,23 @@
-let fs = require('fs');
-let allFile = fs.readFileSync('input.txt').toString().split("\n");
+firstPart = () => {
+    let fs = require('fs');
+    let allFile = fs.readFileSync('input.txt').toString().split("\n");
+    
+    let previousLine = Number.MAX_SAFE_INTEGER;
+    let currentLine = Number.MAX_SAFE_INTEGER;
+    let countOfIncreases = 0;
+    
+    
+    allFile.forEach(line => {
+        previousLine = currentLine;
+        currentLine = parseInt(line);
+    
+        if (currentLine>previousLine)
+            countOfIncreases++;
+    });
+    
+    console.log(`The count of increases is ${countOfIncreases}`);
+}
 
-let previousLine = Number.MAX_SAFE_INTEGER;
-let currentLine = Number.MAX_SAFE_INTEGER;
-let countOfIncreases = 0;
+firstPart();
 
 
-allFile.forEach(line => {
-    previousLine = currentLine;
-    currentLine = parseInt(line);
-
-    if (currentLine>previousLine)
-        countOfIncreases++;
-});
-
-console.log(`The count of increases is ${countOfIncreases}`);
