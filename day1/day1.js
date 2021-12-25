897bf3111f7b16788fc1c0efa6e2ba0c5f81e020
Alvaro Enrique Ruano <alvaro.ruano90@outlook.com>
2021-12-25 10:38:00 -0600

    First problem was accepted

---
+++ day1/day1.js
@@ -0,0 +1,17 @@
+let fs = require('fs');
+let allFile = fs.readFileSync('input.txt').toString().split("\n");
+
+let previousLine = Number.MAX_SAFE_INTEGER;
+let currentLine = Number.MAX_SAFE_INTEGER;
+let countOfIncreases = 0;
+
+
+allFile.forEach(line => {
+    previousLine = currentLine;
+    currentLine = parseInt(line);
+
+    if (currentLine>previousLine)
+        countOfIncreases++;
+});
+
+console.log(`The count of increases is ${countOfIncreases}`);
