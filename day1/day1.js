b3fed23b59249c11ee32fc956dd64cb5d3a7bca0
Alvaro Enrique Ruano <alvaro.ruano90@outlook.com>
2021-12-25 10:57:21 -0600

    Completing second part for day 1

--- day1/day1.js
+++ day1/day1.js
@@ -4,34 +4,58 @@ let allFile = fs.readFileSync('input.txt').toString().split("\n");
 
 // Convert to an integers array
 let allFileIntegers = [];
-allFile.forEach( line => {
+allFile.forEach(line => {
     allFileIntegers.push(parseInt(line));
 });
 
 
-firstPart = () => {    
+firstPart = () => {
     let previousLine = Number.MAX_SAFE_INTEGER;
     let currentLine = Number.MAX_SAFE_INTEGER;
     let countOfIncreases = 0;
-    
-    
+
+
     allFileIntegers.forEach(line => {
         previousLine = currentLine;
         currentLine = line;
-    
-        if (currentLine>previousLine)
+
+        if (currentLine > previousLine)
             countOfIncreases++;
     });
-    
+
     console.log(`The count of increases is ${countOfIncreases}`);
 }
 
 firstPart();
 
-// secondPart = () => {
+secondPart = () => {
+    let countOfIncreases = 0;
+
+    let firstWindow = [0,0,0];
+    let secondWindow = [0,0,0];
+
+    let firstWindowSum = 0;
+    let secondWindowSum = 0;
 
 
-//     let firstWindow = [Number.MAX_SAFE_INTEGER, Number.MAX_SAFE_INTEGER, Number.MAX_SAFE_INTEGER];
-//     let secondWindow = [Number.MAX_SAFE_INTEGER, Number.MAX_SAFE_INTEGER, Number.MAX_SAFE_INTEGER];   
-// }
+    for (let index = 3; index < allFileIntegers.length; index++) {
+        firstWindow[0] = allFileIntegers[index - 3];
+        firstWindow[1] = allFileIntegers[index - 2];
+        firstWindow[2] = allFileIntegers[index - 1];
+
+        secondWindow[0] = allFileIntegers[index - 2];
+        secondWindow[1] = allFileIntegers[index - 1];
+        secondWindow[2] = allFileIntegers[index];
+
+        firstWindowSum = firstWindow.reduce((a, b) => a + b, 0);
+        secondWindowSum = secondWindow.reduce((a, b) => a + b, 0);
+
+        if (secondWindowSum > firstWindowSum)
+            countOfIncreases++;
+    }
+
+    console.log(`The count of Window increases is ${countOfIncreases}`);
+}
+
+secondPart();
 
